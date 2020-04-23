f06b20dfa4274fbba800178665e654e0e28ed0d9
Victor Estrade <victor.estrade@lri.fr>
2020-04-23 23:06:09 +0200

    Add Gaussian Mixture density network loss

--- criterion.py
+++ criterion.py
@@ -7,6 +7,20 @@ from __future__ import unicode_literals
 import torch
 import torch.nn as nn
 
+import numpy as np
+
+
+class ADVLoss(nn.Module):
+    def forward(self, predictions, target):
+        mu, logsigma, mixture = predictions
+        sigma = torch.exp(logsigma)
+        cst = 1./np.sqrt(2. * np.pi)
+        pdf = cst / sigma * torch.exp( -( (target - mu) ** 2 ) / (2. * (sigma ** 2)) )
+        mixture_pdf = torch.sum( pdf * mixture, axis=1)
+        loss = - torch.log(mixture_pdf)
+        mean_loss = torch.mean(loss)
+        return mean_loss
+
 
 class GaussNLLLoss(nn.Module):
     def forward(self, input, target, logsigma):
